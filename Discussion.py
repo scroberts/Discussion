37c53d0ebd3a53a07e7938e588d58d4e0a7e6609
scroberts <scottroberts8314@gmail.com>
2015-07-24 10:30:00 +0900

    updated calls

--- Discussion.py
+++ Discussion.py
@@ -8,11 +8,9 @@ import DISC
 # FDRP2: https://docushare.tmt.org/docushare/dsweb/View/BulletinBoard-302?init=true
 # M1CS: https://docushare.tmt.org/docushare/dsweb/View/BulletinBoard-309?init=true
 
-# mobie = 'MOBIE.html'
-# fdrp1 = 'STR_FDRP1.html'
 
-# DISC.get_discussion(mobie, 'mobie.xlsx')
-# DISC.get_discussion(fdrp1, 'fdrp1.xlsx')
-# DISC.get_discussion('CR173_M1CS.html', 'm1cs.xlsx')
+# DISC.get_discussion('MOBIE.html', 'mobie.xlsx')
+# DISC.get_discussion('STR_FDRP1.html', 'fdrp1.xlsx')
 
+DISC.get_discussion('CR173_M1CS.html', 'm1cs.xlsx')
 DISC.get_discussion('STR_FDRP2.html', 'fdrp2.xlsx')
