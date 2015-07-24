1fa211428b16e190486a5b0bdd88de571e4ae4ec
scroberts <scottroberts8314@gmail.com>
2015-07-24 20:47:16 +0900

    Code for TMT STR FDRP2 Discussion Board

---
+++ Discussion_FDRP2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import DISC
+
+# URLS 
+# First go to DCC and save discussion as .html local file
+# Locations
+# FDRP2: https://docushare.tmt.org/docushare/dsweb/View/BulletinBoard-302?init=true
+
+DISC.get_discussion('STR_FDRP2.html', 'fdrp2.xlsx')
+
