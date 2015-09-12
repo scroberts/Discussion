dc27dfcf75cb71bba88b1728d20ec8d2e68e6a7d
scroberts <scottroberts8314@gmail.com>
2015-09-12 08:03:27 -0700

    Fixed import Config

--- M1CS_PDR.py
+++ M1CS_PDR.py
@@ -8,13 +8,13 @@ import Config as CF
 # URLS 
 # First go to DCC and save discussion as .html local file
 # Locations
-url = 'https://docushare.tmt.org/docushare/dsweb/View/BulletinBoard-309?init=true'
-htmlfile = 'CR173_M1CS.html'
-xlsfile = 'm1cs.xlsx'
+url = 'https://docushare.tmt.org/docushare/dsweb/View/BulletinBoard-331?init=true'
+htmlfile = 'M1CS_PDR.html'
+xlsfile = 'M1CS_PDR_ReviewComments.xlsx'
 
 # Call get_discussion
 # The htmlfile will be stored in the dccfilepath directory and the
 # xlsfile will be stored in the current directory
 
-DISC.get_discussion(url, htmlfile, xlsfile)
+DISC.get_discussion_rowcol(url, htmlfile, xlsfile, 4, 1)
 
